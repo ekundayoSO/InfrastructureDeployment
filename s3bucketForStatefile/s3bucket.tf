77b595c796ec65bc4261de253fea15d1279327f3
Meenahd <123823841+Meenahd@users.noreply.github.com>
2025-03-21 00:07:23 -0500

    Update s3bucket.tf

--- s3bucketForStatefile/s3bucket.tf
+++ s3bucketForStatefile/s3bucket.tf
@@ -1,13 +1,13 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region    = "us-east-1"
-  profile   = "yusuf"
+  profile   = "Dennis"
 }
 
 
 
 resource "aws_s3_bucket" "example" {
-  bucket = "yusuf-jenkins"
+  bucket = "dennis-bucketid"
 
   lifecycle {
     prevent_destroy = true
