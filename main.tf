f39bbc77a60a1b727a5028ac3b439fc30b9389c4
Meenahd <123823841+Meenahd@users.noreply.github.com>
2025-03-20 23:51:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "yusuf-jenkins"
-    key    = "newresource.tfstate"
+    bucket = "dennis-bucketid"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
@@ -104,7 +104,7 @@ resource "aws_instance" "foo" {
   subnet_id = aws_subnet.demo_subnet.id
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
   availability_zone = "us-east-1a"
-  key_name = "devopskeypair"
+  key_name = "LPkp"
   count = 5
 
   tags = {
