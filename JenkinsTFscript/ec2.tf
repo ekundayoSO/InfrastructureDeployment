db1e1af78851072ee42e4427e78ea0d07e913450
Sulaimon Ekundayo <topdandydot@gmail.com>
2025-08-03 18:33:34 +0300

    Update script

--- JenkinsTFscript/ec2.tf
+++ JenkinsTFscript/ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region    = "us-east-1"
-  profile   = "duniola"
+  profile   = "freedom"
 }
 
 
@@ -89,7 +89,8 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.small"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "duniola-keypair"
+  key_name               = "devops-kp"
+  associate_public_ip_address = true
   # user_data = "${file("install_jenkins.sh")}"
 
   tags = {
@@ -105,7 +106,7 @@ resource "null_resource" "name" {
   connection {
     type        = "ssh"
     user        = "ubuntu"
-    private_key = file("~/Downloads/duniola-keypair.pem")
+    private_key = file("~/Downloads/devops-kp.pem")
     host        = aws_instance.ec2_instance.public_ip
   }
 
